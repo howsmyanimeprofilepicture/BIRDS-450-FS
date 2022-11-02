ccb3e3b3c370526750ca87a4b34d747f70d059bd
howsmyanimeprofilepicture <howsmyanimeprofilepicture@gmail.com>
2022-11-02 20:14:43 +0900

    first commit .💕

---
+++ download.sh
@@ -0,0 +1,3 @@
+FILEID="1CgJhLEqTLalfQYz_L-HKMSC_akRBPYFs"
+FILENAME="BIRDS-450-FS.zip"
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=${FILEID}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILEID}" -O ${FILENAME} && rm -rf /tmp/cookies.txt
